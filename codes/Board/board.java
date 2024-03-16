8d325c1d835cd46609ca2ffbb368532783f5f9a2
Sindhu M R <160041251+Sindhu-m-r@users.noreply.github.com>
2024-03-16 07:52:19 +0530

    Create board.java

---
+++ codes/Board/board.java
@@ -0,0 +1,47 @@
+package Board;
+
+import java.util.HashMap;
+
+import jumper.Jumper;
+
+public class board {
+    public int size;
+    public String board[][];
+    public HashMap<String, Jumper> jumpers;
+
+    // jumper
+    public board(int size, HashMap<String, Jumper> jumpers) {
+        this.size = size;
+        this.jumpers = jumpers;
+        board = new String[size][size];
+        int count = 1;
+        for (int row = size - 1; row >= 0; row--) {
+            if (row % 2 != 0) {
+                for (int col = 0; col < size; col++) {
+                    board[row][col] = count + "";
+                    count++;
+                }
+            } else {
+                for (int col = size - 1; col >= 0; col--) {
+                    board[row][col] = count + "";
+                    count++;
+                }
+            }
+        }
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public int getBoardSize() {
+        return this.size;
+    }
+
+}
