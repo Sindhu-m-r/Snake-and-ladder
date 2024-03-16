1f0d528166513d4377cfe261310f55f7c9afcf8d
Sindhu M R <160041251+Sindhu-m-r@users.noreply.github.com>
2024-03-16 07:55:07 +0530

    Create Coordinates.java

---
+++ codes/coordinates/Coordinates.java
@@ -0,0 +1,24 @@
+package coordinates;
+
+public class Coordinates {
+    int row;
+    int col;
+
+    public Coordinates(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public String PrintCoordinater() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[").append(this.row).append(",").append(this.col).append("]");
+        return sb.toString();
+    }
+
+    public int getRow() {
+        return this.row;
+    }
+    public int getcol(){
+        return this.col;
+    }
+}
